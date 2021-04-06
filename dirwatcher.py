cd3200a1c25c85f3ce18fe661a8209451ec2d087
Ben Feder <benfeder@Ben.localdomain>
2021-04-06 11:55:34 -0400

    Log uptime/duration created

--- dirwatcher.py
+++ dirwatcher.py
@@ -10,19 +10,23 @@ import argparse
 import logging
 import signal
 import time
+import datetime
 import os
 
-exit_flag = False
+exit_flag = False  # Create exit flag for infinite loop to occur until True
 
+
+"""Create logger"""
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+logging.basicConfig(
+    format='%(asctime)s: %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logger.setLevel(logging.INFO)
 
 file_handler = logging.FileHandler('dirwatcher.log')
 stream_handler = logging.StreamHandler()
-
-file_handler.setFormatter(formatter)
-stream_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
@@ -42,26 +46,37 @@ def signal_handler(sig_num, frame):
     :return None
     """
     # log the associated signal name
-    logger.warn('Received ' + signal.Signals(sig_num).name)
+    logger.warning('Received ' + signal.Signals(sig_num).name)
+    exit_flag = True
+    end_time = datetime.datetime.now()
+    duration = start_time - end_time
+    logger.info(f"Process took duration: {duration}")
 
 
 def search_for_magic(filename, start_line, magic_string):
     """Search for the magic string in the file"""
-    up_to_line = 0
-    if files_dict[filename] >= 1:
-        up_to_line = files_dict[filename]
+    if not files_dict[filename]:
+        start_line = 0
+    else:
+        start_line = files_dict[filename]
     with open(filename, "r") as f:
-        for line in f:
-            read = line.read()
-            up_to_line += 1
-            if magic_string in read:
-                files_dict[filename] = up_to_line
+        for i, line in enumerate(f):
+            if i >= start_line:
+                read = line.read()
+                start_line += 1
+                if magic_string in read:
+                    logger.info(
+                        f"""{magic_string} was found in {filename} at line
+                    {start_line}.""")
+                files_dict[filename] = start_line
 
 
 def watch_directory(path, magic_string, extension, interval):
+    """Watch directory by looking at all files in directory for magic string"""
     for filename in os.listdir(path):
         if filename.endswith(extension):
             search_for_magic(filename, files_dict[filename], magic_string)
+    time.sleep(interval)
 
 
 # def scan_single_file():
@@ -83,11 +98,12 @@ def watch_directory(path, magic_string, extension, interval):
 
 
 def create_parser():
+    """Create arg parser"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dirwatch', help="Monitors given directory named")
+    parser.add_argument('--dir', help="Monitors given directory named")
     parser.add_argument('--ext', help="Searches files with given extension")
     parser.add_argument('--int', help="Searches dir every given interval")
-    parser.add_argument('magic_string', help="""Must input a string to search
+    parser.add_argument('magic', help="""Must input a string to search
     for in files under given directory""")
 
     args = parser.parse_args()
@@ -97,6 +113,7 @@ def create_parser():
 
 def main(args):
 
+"""Create a namespace from create parser, then watch directory"""
     ns = create_parser()
 
     # Hook into these two signals from the OS
@@ -105,22 +122,20 @@ def main(args):
     signal.signal(signal.SIGHUP, signal_handler)
     # Now my signal_handler will get called if OS sends
     # either of these to my process.
-
     while not exit_flag:
-        try:
-            if not ns.dirwatch:
+        start_time = datetime.datetime.now()
+       try:
+            if not ns.dir:
                 logger.error("No directory given to watch.")
             else:
-                watch_directory(f"{ns.dirwatch}/",
-                                ns.magic_string, ns.ext, ns.int)
+                watch_directory(f"{ns.dir}/", ns.magic, ns.ext, ns.int)
         except Exception as e:
             # This is an UNHANDLED exception
             # Log an ERROR level message here
             logger.error(e)
 
         # put a sleep inside my while loop so I don't peg the cpu usage at 100%
-        if ns.int:
-            time.sleep(ns.int)
+        time.sleep(ns.int)
 
     # final exit point happens here
     # Log a message that we are shutting down
